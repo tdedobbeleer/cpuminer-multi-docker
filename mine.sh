404b1f74d1d154bd9f00dda09a5dfc037f4d0158
tdedobbeleer <tdedobbeleer@gmail.com>
2023-10-25 19:23:55 +0200

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CONFIG=/opt/cpuminer/config.json
+
+if [ -f "$CONFIG" ]; then 
+  echo
+  echo ">>> No config file found, specify in ${CONFIG}"
+  exit 1
+fi 
+
+echo
+echo "****************************************************"
+echo "Building cpuminer-multi"
+echo "****************************************************"
+echo "Compiling for current host processor..."
+
+#Make it baby
+cd /tmp/cpuminer-multi
+./build.sh
+cp cpuminer /usr/bin
+
+#Cleanup
+RUN rm -rf /tmp/*
+
+echo
+echo "****************************************************"
+echo "Mining..."
+echo "****************************************************"
+
+exec /usr/bin/cpuminer --config=$CONFIG
